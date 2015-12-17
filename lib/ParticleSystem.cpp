30982083d4ffb66c9a83ea1098aa78d8de76ddad
Pasindu Gunasekara <pasindugunasekara@hotmail.com>
2015-12-16 19:38:06 -0500

    roberto gave me the wrong numbers

--- lib/ParticleSystem.cpp
+++ lib/ParticleSystem.cpp
@@ -37,9 +37,9 @@ void ParticleSystem::initialize(int i)
 	system[i].lifeSpan = 1.0f;//all particles will have a lifespan between 0 and 1
 	system[i].decay = float(rand()%100)/1000.0f+0.005f;//Random decay for every drop
 	
-	system[i].x = float((rand()%(12+12))-12);///Pick any point between 0 and 50 for the x axis
+	system[i].x = float((rand()%(26+26))-26);///Pick any point between 0 and 50 for the x axis
 	system[i].y = 7.0f;//Every particle will start at a constant height
-	system[i].z = float((rand()%(12+12))-12);///pick any point on the z axis
+	system[i].z = float((rand()%(26+26))-26);///pick any point on the z axis
 	system[i].gravity = -0.8f;//the drops will fall at random speeds
 	system[i].speed = ySpeed;
 }
